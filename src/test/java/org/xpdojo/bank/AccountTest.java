0c3fa4838454ca8dfff46641cc73c3ef2797799b
alimovazat <alimov.azat@gmail.com>
2020-07-16 10:28:12 -0400

    all tests

--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
+	//@Disabled
     public void CheckTheBalance() {
         Account account = new Account();
         assertThat(account.balance()).isEqualTo(0);
